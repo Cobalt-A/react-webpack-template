faf5636d0f273c63b6df003a5f61b463df090b67
Cobalt-A <nikitos.gg.yt@gmail.com>
2024-07-02 13:46:59 +0400

    feat: prod/dev configs

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.scss'
+import styles from './index.module.scss'
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const asd = 'werwetrytfghfghyr'
+
 root.render(
   <React.StrictMode>
-    
+    <p className={styles.test}>{asd}dfgdfgdfg</p>
   </React.StrictMode>
 );
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    console.log(123);
-    
     navigator.serviceWorker
     .register("/service-worker.js")
     .then((registration) => {
---
+++ webpack.dev.config.js
@@ -0,0 +1,99 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DotenvWebpackPlugin = require("dotenv-webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = (env, args) => {
+  const mode = args.mode;
+  const envFile = path.resolve(__dirname, `.env.${args.mode}`);
+
+  return {
+    mode: "development",
+    stats: "minimal",
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "js/[name].[hash].js",
+      chunkFilename: "js/[name].[contenthash].js",
+      publicPath: "/",
+      clean: true
+    },
+    devtool: 'source-map',
+    devServer: {
+      port: 3000,
+      open: false,
+      hot: true,
+      historyApiFallback: true,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+      extensions: [".scss", ".sass", ".ts", ".tsx", ".js", ".jsx"],
+    },
+    plugins: [
+      new ESLintPlugin({
+        files: 'src/**/*.ts',
+      }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        favicon: path.resolve(__dirname, "public", "favicon.ico"),
+      }),
+      new DotenvWebpackPlugin({
+        path: envFile,
+        safe: true,
+        systemvars: true,
+        silent: true,
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            globOptions: {
+              ignore: ["**/index.html"],
+            },
+            from: path.resolve(__dirname, "public"),
+            to: path.resolve(__dirname, "build"),
+          },
+        ],
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                esModule: true,
+                modules: true,
+              },
+            },
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|jpeg|gif|ico|jpg|woff(2)?|eot|ttf|otf|svg)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "media",
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(ts|js)x?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
+};
